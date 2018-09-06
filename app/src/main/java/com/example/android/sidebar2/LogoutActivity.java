220bfcb98cf7792ddf71dea1c2069ed2ad2d745c
douglasduncan <douglas.duncan@btinternet.com>
2018-09-06 22:57:03 +0100

    logout completed

--- app/src/main/java/com/example/android/sidebar2/LogoutActivity.java
+++ app/src/main/java/com/example/android/sidebar2/LogoutActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sidebar2;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,11 +12,12 @@ public class LogoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
-
+        final Intent logout_intent = new Intent(this, InitialCheck.class);////////////create an intent to change to the main activity class
         Log.w("logout", "logging out");
         SharedPreferences.Editor clear_preferences = getSharedPreferences("InitialPrefs", MODE_PRIVATE).edit();
         clear_preferences.clear();
         clear_preferences.apply();
         Log.w("logout", "cleared preferences");
+        startActivity(logout_intent);//////open the initial check activity class
     }
 }
