860d172279380697b0e345fbe00a2cd929575810
douglasduncan <douglas.duncan@btinternet.com>
2018-09-06 22:48:25 +0100

    added removal of shared preferences on logout

--- app/src/main/java/com/example/android/sidebar2/LogoutActivity.java
+++ app/src/main/java/com/example/android/sidebar2/LogoutActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sidebar2;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LogoutActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LogoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
+
+        Log.w("logout", "logging out");
+        SharedPreferences.Editor clear_preferences = getSharedPreferences("InitialPrefs", MODE_PRIVATE).edit();
+        clear_preferences.clear();
+        clear_preferences.apply();
+        Log.w("logout", "cleared preferences");
     }
 }
